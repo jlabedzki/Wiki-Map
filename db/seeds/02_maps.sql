e1a11497727f65c7e54384d5f8df3130a4ddcf20
Johnny <johnnylabedzki@hotmail.com>
2021-09-18 10:52:38 -0700

    Create schemas and seeds for all 4 tables

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,4 @@
+-- Maps table seeds here
+
+INSERT INTO maps (creator_id, title, longitude_1, latitude_1, longitude_2, latitude_2, category)
+VALUES(2, 'Best Groomers In Town', 49.321123176418546, -123.04224325741416, 49.22143848014641, -123.20257498348838, 'Pets');
