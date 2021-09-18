5966f0e52fa9ba6be09bc3b5fb0633cc5bcc2fb4
Johnny <johnnylabedzki@hotmail.com>
2021-09-18 12:35:39 -0700

    Move db connection from server.js to db.js

--- server.js
+++ server.js
@@ -5,16 +5,12 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
+const { db, dbParams } = require('./lib/db.js')
 const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require('morgan');
 
-// PG database client/connection setup
-const { Pool } = require('pg');
-const dbParams = require('./lib/db.js');
-const db = new Pool(dbParams);
-db.connect();
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
