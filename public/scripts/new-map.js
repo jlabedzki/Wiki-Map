6e93aee4d7eae37999446a372ce088940f76cef9
Johnny <johnnylabedzki@hotmail.com>
2021-09-20 11:13:17 -0700

    Setup functionality of create new map button

--- public/scripts/new-map.js
+++ public/scripts/new-map.js
@@ -1,5 +1,11 @@
 (function ($) {
   $(() => {
     const $createMap = $('#create-new-map');
+    $createMap.click(() => {
+      $('.new-map').show();
+      $('.main-container').hide();
+      $createMap.hide();
+    })
+
   });
 })(jQuery);
