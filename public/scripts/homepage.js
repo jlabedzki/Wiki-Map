decccba7a6a09759b0d8478d456560b734664fe8
Johnny <johnnylabedzki@hotmail.com>
2021-09-20 16:41:10 -0700

    Add id value to each map in map list.

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -1,9 +1,6 @@
 (function ($) {
   $(() => {
     let mapID = 1;
-    //foo.click(() => {
-    // mapID = $(this).id
-    // })
 
     $('.new-map').hide();
     displayMapByID(mapID);
@@ -12,27 +9,35 @@
 
   const displayListOfMaps = () => {
     $.get('/maps', data => {
-      console.log(data.maps);
-
-      let id;
-      let title;
-
+      let counter = 1;
       for (const map of data.maps) {
-        $('#list-of-maps').append(`<li>${map.title}</li>`)
+        $('#list-of-maps').append(`<li value="${map.id}" id="list-item-${counter}">${map.title}</li>`);
+        counter++;
       }
-    });
+    })
+      .then((data) => {
+        let counter = 1;
+        for (const map of data.maps) {
+          $(`#list-item-${counter}`).click(function () {
+            mapID = $(this).val();
+            displayMapByID(mapID);
+          });
+          counter++;
+        }
+      });
   };
 
   const displayMapByID = (mapID) => {
     $.get(`/maps/${mapID}`, data => {
-      console.log(data.map[0].longitude_1);
       const coordinates = [data.map[0].longitude_1, data.map[0].latitude_1, data.map[0].longitude_2, data.map[0].latitude_2];
+
       loadMapByCoords(coordinates);
     });
   }
 
   const loadMapByCoords = (coords) => {
     const mymap = L.map('current-mapid');
+
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data, imagery &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</a>',
       maxZoom: 18,
@@ -42,5 +47,12 @@
       [coords[0], coords[1]],
       [coords[2], coords[3]]
     ]);
+  }
+
+  const reloadMap = (map) => {
+    if (map) {
+      map.off()
+      map.remove();
+    }
   }
 })(jQuery);
